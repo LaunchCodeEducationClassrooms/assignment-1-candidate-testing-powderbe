c7a9fa21681c86a095c66f23ea0ad2d2f1f3f903
powderbe <bpowderlyii@gmail.com>
2021-02-20 10:42:10 +0000

    1.1b Candidate Name Space

--- candidate-testing.js
+++ candidate-testing.js
@@ -17,7 +17,7 @@ let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Candidate Name: ");
+candidateName = input.question("Candidate Name:");
 }
 
 function askQuestion() {
